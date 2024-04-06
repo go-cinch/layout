deaf4d880791db047987fd71d7a0117325595a54
piupuer <piupuer@gmail.com>
2024-04-06 14:01:18 +0800

    [feat]add unit test

---
+++ internal/tests/service/game_test.go
@@ -0,0 +1,73 @@
+package service
+
+import (
+	"context"
+	"github.com/go-cinch/common/proto/params"
+	"github.com/go-cinch/layout/api/game"
+	"github.com/go-cinch/layout/internal/tests/mock"
+	"github.com/google/uuid"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func TestGameService_CreateGame(t *testing.T) {
+	s := mock.GameService()
+	ctx := context.Background()
+	userID := uuid.NewString()
+	ctx = mock.NewContextWithUserId(ctx, userID)
+
+	_, err := s.CreateGame(ctx, &game.CreateGameRequest{
+		Name: "game1",
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = s.CreateGame(ctx, &game.CreateGameRequest{
+		Name: "game2",
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	res1, _ := s.FindGame(ctx, &game.FindGameRequest{
+		Page: &params.Page{
+			Disable: true,
+		},
+	})
+	if res1 == nil || len(res1.List) != 2 {
+		t.Error("res1 len must be 2")
+		return
+	}
+	res2, err := s.GetGame(ctx, &game.GetGameRequest{
+		Id: res1.List[0].Id,
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if res2.Name != res1.List[0].Name {
+		t.Errorf("res2.Name must be %s", res1.List[0].Name)
+		return
+	}
+	_, err = s.DeleteGame(ctx, &params.IdsRequest{
+		Ids: strings.Join([]string{
+			strconv.FormatUint(res1.List[0].Id, 10),
+			strconv.FormatUint(res1.List[1].Id, 10),
+		}, ","),
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	res3, _ := s.FindGame(ctx, &game.FindGameRequest{
+		Page: &params.Page{
+			Disable: true,
+		},
+	})
+	if res3 == nil || len(res3.List) != 0 {
+		t.Error("res3 len must be 0")
+		return
+	}
+}
